70a06af4d33ce0b7840f0c4ca062540c10592b62
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-01-30 14:40:29 +0530

    chapter3
    1.Create Script
    2. React Element
    3. core of react
    4. jsx
    5. react component
    7. component compsitions

---
+++ chapter3 - Laying the foundation/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import ReactDOM  from 'react-dom/client';
+
+
+// there are two ways for react Component
+// 1. class based
+// 2. function based
+
+// const Heading = () => {
+//     return <h1 className="heading">Hi i am heading in functional component</h1>
+// }
+// const Heading = () => <h1>Hi i am heading</h1>;
+
+// const Heading = () => (
+//     <div id="container">
+//     <h1 className="head">Hi i am heading without  return keyword</h1>
+//     </div>
+// )
+
+const Title = () => (
+    <h1 className="title">Namaste React by Akshay saini</h1>
+);
+
+
+// component compositions
+const Para = () => (
+    <div className="container">
+        <Title/>
+        <h2>React</h2>
+        <p>hi this is react and develope by facebook developers
+        </p>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Para/>);
+
