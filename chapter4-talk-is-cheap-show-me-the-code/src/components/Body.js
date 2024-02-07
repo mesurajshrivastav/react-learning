c3bd54f8b4a060d23cad1605fcbeea078621c689
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-02-07 18:47:31 +0530

    Work with live swiggy api data

--- chapter4-talk-is-cheap-show-me-the-code/src/components/Body.js
+++ chapter4-talk-is-cheap-show-me-the-code/src/components/Body.js
@@ -1,25 +1,36 @@
 import ResCards from "./ResCards";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 //creating main body
 
 const Body = () => {
-  const [listOfRestaurents, setListOfRestaurents] = useState(resList);
-  const handleFilter = function(){
+  const [listOfRestaurents, setListOfRestaurents] = useState([]);
+  const handleFilter = function () {
     const filteredList = listOfRestaurents.filter(
-      (res)=> res.info.avgRating > 4
+      (res) => res.info.avgRating > 4.5
     );
     setListOfRestaurents(() => filteredList);
-  }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // optional chaining
+    setListOfRestaurents(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   return (
     <div className="body-container">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={handleFilter}
-        >
+        <button className="filter-btn" onClick={handleFilter}>
           Top Rated Restaurents
         </button>
       </div>
