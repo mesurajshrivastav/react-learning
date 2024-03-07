de6fc85d2c476d6d0f985058c86bddeabd6c38f5
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-03-07 18:35:53 +0530

    created my first custom hook in react

--- chapter4-talk-is-cheap-show-me-the-code/src/components/RestaurentMenu.js
+++ chapter4-talk-is-cheap-show-me-the-code/src/components/RestaurentMenu.js
@@ -1,28 +1,11 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurentMenu from "../utils/useRestaurentMenu";
 
 const RestaurentMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
 
-  const {resId} = useParams();
-  
-
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(
-    MENU_API + resId
-    );
-
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurentMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
@@ -30,7 +13,9 @@ const RestaurentMenu = () => {
     resInfo?.cards[0]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+  console.log(itemCards);
 
   return (
     <div className="menu">
@@ -39,15 +24,19 @@ const RestaurentMenu = () => {
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
       <h2>{avgRating} stars</h2>
-      <h3></h3>
+
       <div className="menu">
         <h2>Menu</h2>
         <ul>
-          {itemCards.map((item) => (
-            <li key={item.card.info.id}>
-              {item.card.info.name} - &#8377; {item.card.info.price / 100}
-            </li>
-          ))}
+          {itemCards &&
+            itemCards.length > 0 &&
+            itemCards.map((item) => {
+              return (
+                <li key={item.card.info.id}>
+                  {item.card.info.name} - &#8377; {item.card.info.price / 100}
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
