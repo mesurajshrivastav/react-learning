224a39d70250b7c7adce8fc26416bd90980de4a3
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-03-07 11:20:11 +0530

    learned how the lifecycle method work in class based component

--- chapter4-talk-is-cheap-show-me-the-code/src/components/About.js
+++ chapter4-talk-is-cheap-show-me-the-code/src/components/About.js
@@ -1,15 +1,28 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About us</h1>
-      <h2>This is Namaste React webseries</h2>
-      {/* <User name={"Suraj Shrivastav"} location ={"Delhi"} learning ={"Javascript"}/> */}
-      <UserClass name={"Suraj"} location={"New Delhi"} learning={"React"} />
-    </div>
-  );
-};
+
+class About extends React.Component {
+    constructor() {
+        super()
+        console.log("parent constructor called");
+    }
+
+    componentDidMount(){
+        console.log("parent componentDidMount called");
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log("parent render called")}
+              <h1>About us</h1>
+              <h2>This is Namaste React webseries</h2>
+              <UserClass name={"Suraj"} location={"New Delhi"} learning={"React"} />
+            </div>
+          );
+    }
+} 
 
 export default About;
--- chapter4-talk-is-cheap-show-me-the-code/src/components/UserClass.js
+++ chapter4-talk-is-cheap-show-me-the-code/src/components/UserClass.js
@@ -3,13 +3,20 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    // console.log(props);
 
     this.state = {
       count: 0,
       count2: 2,
     };
+
+    console.log("child constructor called");
   }
+
+  componentDidMount() {
+    console.log("child componentDidMount called");
+  }
+
   render() {
     const { count, count2 } = this.state;
 
@@ -17,12 +24,17 @@ class UserClass extends React.Component {
     return (
       <div className="user-card">
         <h2>count : {count}</h2>
-        <button onClick ={()=> {
+        <button
+          onClick={() => {
             this.setState({
-                count: this.state.count +1,
-                count2: this.state.count2*2,
-            })
-        }}>count increase</button>
+              count: this.state.count + 1,
+              count2: this.state.count2 * 2,
+            });
+          }}
+        >
+          count increase
+        </button>
+        {console.log("child render called")}
         <h2>count : {count2}</h2>
         <h3>Name : {name}</h3>
         <h3>Location : {location}</h3>
