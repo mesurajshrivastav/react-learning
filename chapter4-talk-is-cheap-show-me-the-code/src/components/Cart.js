f24727cb6e662dfdec94dc4ca121704b81bdff18
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-03-23 11:47:13 +0530

    added a clear functionality to the cart component

--- chapter4-talk-is-cheap-show-me-the-code/src/components/Cart.js
+++ chapter4-talk-is-cheap-show-me-the-code/src/components/Cart.js
@@ -1,13 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Itemlist from "./ItemList";
+import { clearCart } from "../utils/reduxSlice/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <>
       <div className="text-center m-4 p-4">
         <h1 className="text-2xl font-bold">Cart</h1>
-        <div><Itemlist items={cartItems}/></div>
+        <div className="w-6/12 m-auto">
+          <button
+            onClick={handleClearCart}
+            className="p-2 bg-black text-white rounded-lg"
+          >
+            Clear Cart
+          </button>
+          <Itemlist items={cartItems} />
+        </div>
       </div>
     </>
   );
